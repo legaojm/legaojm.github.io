e95e31cfdc86860c9ba645a524ac2ed91849142f
legaojm <117961175+legaojm@users.noreply.github.com>
2022-11-11 18:14:52 +0800

    web

---
+++ js/livechat.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+document.writeln("<div style=\"width:155px;height:245px;float:right; z-index:999;position:fixed !important; top/**/:220px; position:absolute; top:expression(offsetParent.scrollTop+400);right:0px;\"> ");
+document.writeln('<script language="javascript" src="http://pkt.zoosnet.net/JS/LsJS.aspx?siteid=PKT21646611&float=0&lng=en"></script>');;
+document.writeln("<a href=\"javascript:void(0)\" onclick=openZoosUrl(\'chatwin\')  target=\"_self\"><img src=\"/js/swt.gif\" border=\"0\"></a>");
+document.writeln("</div>");
